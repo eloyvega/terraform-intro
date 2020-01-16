11deb97e525d0025492efb9cb2054354ccf9f331
Eloy Vega <eloy.vega@wizeline.com>
2020-01-16 11:23:44 -0600

    Comment out the backend

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "bootcamp-eloyvega-terraform-state"
-    key    = "fullstack/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "bootcamp-eloyvega-terraform-state"
+#     key    = "fullstack/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
